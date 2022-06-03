a59b18d29c3e73228c3286aaaeeb41325ab1fe2b
Mobin Skaria <mobin0@berkeley.edu>
2022-06-03 16:06:01 -0700

    First Commit - Overloading C#

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Overloading
+{
+    internal class Program
+    {
+        public static int Add(int a, int b) {
+            return a + b;
+        
+        }
+
+        public static double Add(double a, double b)
+        {
+
+
+            return a + b;
+
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            int result = a + b;
+            if (c) {
+                return result +"$";
+            }
+            return "{a+b}";
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1.11, 2.22));
+            Console.WriteLine(Add(1,1,true));
+            Console.WriteLine("Add Overloading Complete");
+        }
+    }
+}
